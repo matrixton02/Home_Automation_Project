d8e67a22f772a341c179eae37fd7da987a1c62a4
matrixton02 <yashasvi21022005@gmail.com>
2024-09-23 09:25:28 +0530

    uploading set up files for nltk setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import nltk
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
